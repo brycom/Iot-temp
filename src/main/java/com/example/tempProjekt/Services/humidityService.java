27e01698ae6cfc2b74fcb49dbdd8fb6f7116a4e1
brycom <mathiasbrynolf20@gmail.com>
2024-10-03 11:24:08 +0200

    benidorm

--- src/main/java/com/example/tempProjekt/Services/humidityService.java
+++ src/main/java/com/example/tempProjekt/Services/humidityService.java
@@ -1,48 +1,47 @@
 package com.example.tempProjekt.Services;
 
+import com.example.tempProjekt.models.Humidity;
+import com.example.tempProjekt.repository.humidityRepository;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
-
 import org.springframework.stereotype.Service;
-
-import com.example.tempProjekt.models.Humidity;
-import com.example.tempProjekt.repository.humidityRepository;
 
 @Service
 public class humidityService {
 
-    private final humidityRepository humidityRepository;
-    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+  private final humidityRepository humidityRepository;
+  private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
 
-    public humidityService(humidityRepository humidityRepository) {
-        this.humidityRepository = humidityRepository;
-    }
+  public humidityService(humidityRepository humidityRepository) {
+    this.humidityRepository = humidityRepository;
+  }
 
-    public Humidity newHumidity(float humidityin) {
+  public Humidity newHumidity(float humidityin) {
+    LocalDate date = LocalDate.now();
+    LocalTime time = LocalTime.now();
+    String timeString = time.format(formatter);
 
-        LocalDate date = LocalDate.now();
-        LocalTime time = LocalTime.now();
-        String timeString = time.format(formatter);
+    Humidity humidity = new Humidity(humidityin, timeString, date.toString());
+    return humidityRepository.save(humidity);
+  }
 
-        Humidity humidity = new Humidity(humidityin, timeString, date.toString());
-        return humidityRepository.save(humidity);
+  public List<Humidity> getTodaysHumidity() {
+    String date = /*  LocalDate.now().toString() */LocalDate
+      .of(2024, 9, 24)
+      .toString();
 
-    }
+    return humidityRepository.todaysHumidity(date);
+  }
 
-    public List<Humidity> getTodaysHumidity() {
-        String date = LocalDate.now().toString();
+  public List<Humidity> getHumidityForHour() {
+    String date = /* LocalDate.now().toString() */LocalDate
+      .of(2024, 9, 24)
+      .toString();
+    LocalTime time = /* LocalTime.now() */LocalTime.of(14, 15);
+    int hour = time.getHour();
 
-        return humidityRepository.todaysHumidity(date);
-    }
-
-    public List<Humidity> getHumidityForHour() {
-        String date = LocalDate.now().toString();
-        LocalTime time = LocalTime.now();
-        int hour = time.getHour();
-
-        return humidityRepository.humidityForHour(date, hour);
-    }
-
+    return humidityRepository.humidityForHour(date, hour);
+  }
 }
--- src/main/java/com/example/tempProjekt/Services/tempService.java
+++ src/main/java/com/example/tempProjekt/Services/tempService.java
@@ -1,50 +1,49 @@
 package com.example.tempProjekt.Services;
 
+import com.example.tempProjekt.models.Temp;
+import com.example.tempProjekt.repository.tempRepository;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
-
-import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Service;
-
-import com.example.tempProjekt.models.Temp;
-import com.example.tempProjekt.repository.humidityRepository;
-import com.example.tempProjekt.repository.tempRepository;
 
 @Service
 public class tempService {
 
-    private final tempRepository tempRepository;
+  private final tempRepository tempRepository;
 
-    public tempService(tempRepository tempRepository) {
-        this.tempRepository = tempRepository;
-    }
+  public tempService(tempRepository tempRepository) {
+    this.tempRepository = tempRepository;
+  }
 
-    public Temp newTemp(float tempIn) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
-        LocalDate date = LocalDate.now();
-        LocalTime time = LocalTime.now();
-        String timeString = time.format(formatter);
-        Temp temp = new Temp(tempIn, timeString, date.toString());
-        return tempRepository.save(temp);
+  public Temp newTemp(float tempIn) {
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+    LocalDate date = LocalDate.now();
+    LocalTime time = LocalTime.now();
+    String timeString = time.format(formatter);
+    Temp temp = new Temp(tempIn, timeString, date.toString());
+    return tempRepository.save(temp);
+  }
 
-    }
+  public Iterable<Temp> getAllTemps() {
+    return tempRepository.findAll();
+  }
 
-    public Iterable<Temp> getAllTemps() {
-        return tempRepository.findAll();
-    }
+  public List<Temp> getTodaysTemps() {
+    String date = /* LocalDate.now().toString() */LocalDate
+      .of(2024, 9, 24)
+      .toString();
 
-    public List<Temp> getTodaysTemps() {
-        String date = LocalDate.now().toString();
+    return tempRepository.todaysTemp(date);
+  }
 
-        return tempRepository.todaysTemp(date);
-    }
-
-    public List<Temp> getTempsForHour() {
-        String date = LocalDate.now().toString();
-        LocalTime time = LocalTime.now();
-        int hour = time.getHour();
-        return tempRepository.tempsForHour(date, hour);
-    }
+  public List<Temp> getTempsForHour() {
+    String date = /* LocalDate.now().toString() */LocalDate
+      .of(2024, 9, 24)
+      .toString();
+    LocalTime time = /* LocalTime.now() */LocalTime.of(14, 15);
+    int hour = time.getHour();
+    return tempRepository.tempsForHour(date, hour);
+  }
 }
